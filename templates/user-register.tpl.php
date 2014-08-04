8b8b62666a95966eb222ea891e74bdf113d7b1cf
rod <rod.higgins@mail.com>
2014-08-04 11:08:24 +1000

    Inital build

--- templates/user-register.tpl.php
+++ templates/user-register.tpl.php
@@ -1,6 +1,25 @@
 <div class="blueFilterContainerTop"></div>
 <div class="secondary-main-content">
+   <div style="margin-top: 75px;"></div>
 	<div id="story-header-title">Register</div>
+	
+
+	<div class="register-content">
+		<p>Can you tell us more about the World War I soldiers whose diaries we hold? 
+
+<p>The State Library holds 1200 volumes of diaries written by 550 men and women who served in the First World War. We know very little about many of them, apart from their personal experiences detailed in their writing. With your help, we can add stories, photos and momentoes to the collections we hold. 
+
+<p>You must be logged in to add a story or upload a photo.
+
+<p>Register or log in using your email of facebook account to add your own story or photo about a World War I diarist in our collection.
+
+<p>Please add the following statement below the login and register fields:
+
+<p><strong>Copyright and Ownership</strong>
+
+<p>When you upload content, you are giving the State Library of NSW permission to publish. Please read our Terms and Conditions before sharing your content. 
+	</div>	
+	
 	<?php print $messages; ?>
 	<div class="user-login-fb-container">
 		<!-- Custom login form -->
@@ -19,4 +38,5 @@
 	?>
 	
 	<a href="/user/login">Login</a>
+	<div style="margin-bottom: 50px;"></div>
 </div>
